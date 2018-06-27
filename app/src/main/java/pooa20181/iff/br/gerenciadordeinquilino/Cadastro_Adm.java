f44cd054ec6abd287ece74a59095ef1e7133a92d
Mariana Vieira <marianaovieira1@gmail.com>
2018-06-27 10:31:20 -0300

    Criação da tela de cadastro de administrador e intents entre telas
    Signed-off-by: Mariana Vieira <marianaovieira1@gmail.com>

---
+++ app/src/main/java/pooa20181/iff/br/gerenciadordeinquilino/Cadastro_Adm.java
@@ -0,0 +1,15 @@
+package pooa20181.iff.br.gerenciadordeinquilino;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Cadastro_Adm extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro__adm);
+
+
+    }
+}
--- app/src/main/java/pooa20181/iff/br/gerenciadordeinquilino/Login.java
+++ app/src/main/java/pooa20181/iff/br/gerenciadordeinquilino/Login.java
@@ -1,7 +1,10 @@
 package pooa20181.iff.br.gerenciadordeinquilino;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button chama_cadastro = (Button) findViewById(R.id.btnCadastrarse);
+        chama_cadastro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Login.this, Cadastro_Adm.class);
+                startActivity(intent);
+            }
+        });
     }
 }
